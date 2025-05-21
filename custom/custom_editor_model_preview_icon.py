59ab04b65487557a62fb49ba70755a2614099991
Edward <heweihui1@outlook.com>
2025-05-21 21:15:40 +0800

    2025_05_21基础装备功能完成，移入信息缺少UI设计还没弄

--- custom/custom_editor_model_preview_icon.py
+++ custom/custom_editor_model_preview_icon.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134221191: {'path': 'UP1ModelPreview/134221191', 'key': 134221191, 'size': '100X68'}, 134231044: {'path': 'UP1ModelPreview/134231044', 'key': 134231044, 'size': '100X68'}}
