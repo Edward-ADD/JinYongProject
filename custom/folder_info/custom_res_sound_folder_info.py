f28324834ee08d54bea34490cb8bdd610eb838f3
Edward <heweihui1@outlook.com>
2025-05-22 21:07:52 +0800

    2025_5_22修复了卡牌BUG，追加了移入显示神器信息，修改了部分UI交互效果，初步的出怪逻辑（还有BUG）

--- custom/folder_info/custom_res_sound_folder_info.py
+++ custom/folder_info/custom_res_sound_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {'134222531': (2147483647, 0), '134232016': (2147483647, 1), '134247903': (2147483647, 2)}}
