47b14e835ad31b17d010ca074bfc1711d7ad79c3
Edward <heweihui1@outlook.com>
2025-05-23 21:39:36 +0800

    2025_05_23追加了一些UI美术效果，调整了精英怪和BOSS的物编功能，精英怪，小BOSS，大BOSS技能搬运

--- custom/custom_editor_model_preview_icon.py
+++ custom/custom_editor_model_preview_icon.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {134221191: {'path': 'UP1ModelPreview/134221191', 'key': 134221191, 'size': '100X68'}, 134231044: {'path': 'UP1ModelPreview/134231044', 'key': 134231044, 'size': '100X68'}}
+data = {134221191: {'path': 'UP1ModelPreview/134221191', 'key': 134221191, 'size': '100X68', 'source_path': 'C:\\Users\\LZL\\AppData\\Local\\Temp\\134221191.png'}, 134231044: {'path': 'UP1ModelPreview/134231044', 'key': 134231044, 'size': '100X68', 'source_path': 'C:\\Users\\LZL\\AppData\\Local\\Temp\\134231044.png'}}
