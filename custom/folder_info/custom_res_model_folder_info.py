ccd5d44aa6edc403f6b7b372cdb8cd04cd8a8abd
Edward <heweihui1@outlook.com>
2025-06-10 21:07:27 +0800

    2025_06_10武功配置5

--- custom/folder_info/custom_res_model_folder_info.py
+++ custom/folder_info/custom_res_model_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'mBm7LjVUEfCt6FOrgOSSj9', '角色')], 'd': {134221191: ('mBm7LjVUEfCt6FOrgOSSj9', 1), 134231044: ('mBm7LjVUEfCt6FOrgOSSj9', 0)}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'mBm7LjVUEfCt6FOrgOSSj9', '角色')], 'd': {134221191: ('mBm7LjVUEfCt6FOrgOSSj9', 1), 134231044: ('mBm7LjVUEfCt6FOrgOSSj9', 0), 134237693: (2147483647, 1)}}
