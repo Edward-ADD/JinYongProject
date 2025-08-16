17b3283c066b573b27f2b510edb6cd0b72bbd4ab
Edward <heweihui1@outlook.com>
2025-08-16 21:40:02 +0800

    2025_08_16初步的每日任务框架，优化了新手教程存档，追加了新手教程语音

--- custom/folder_info/custom_sound_folder_info.py
+++ custom/folder_info/custom_sound_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {134222531: (2147483647, 0), 134232016: (2147483647, 1)}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, '4PGBxXqZEfCoTtknqsucn4', '新手教程')], 'd': {134253153: ('4PGBxXqZEfCoTtknqsucn4', 0), 134239304: ('4PGBxXqZEfCoTtknqsucn4', 1), 134237002: ('4PGBxXqZEfCoTtknqsucn4', 2), 134265837: ('4PGBxXqZEfCoTtknqsucn4', 3), 134280547: ('4PGBxXqZEfCoTtknqsucn4', 4), 134251758: ('4PGBxXqZEfCoTtknqsucn4', 5), 134228374: ('4PGBxXqZEfCoTtknqsucn4', 6), 134266377: ('4PGBxXqZEfCoTtknqsucn4', 7), 134222531: (2147483647, 1), 134232016: (2147483647, 2)}}
